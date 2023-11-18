e3baea7324f87ad30b1db336d284f4a7ab3bfb5f
tlsena <thiagosena.d.j@gmail.com>
2023-11-17 21:12:27 -0300

    Arquivos das ultimas 2 semanas + Ignore

---
+++ Semana-1/PrimeirosPassos/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Hello, World!");
+
+#region Tipos de dados 
+
+int tipoInteiro;
+double tipoDouble;
+float tipoFloat;
+long tipoLong;
+string tipoString;
+
+#endregion
+
+tipoInteiro = int.MaxValue;
+tipoLong = long.MaxValue;
+
+tipoString = "100";
+tipoInteiro = int.Parse(tipoString);
+
+Console.WriteLine("O maior número inteiro é: " + tipoInteiro);
+Console.WriteLine("O maior número long é: " + tipoLong);
+
+#region Operadores
+tipoDouble = tipoInteiro + tipoLong;
+
+#endregion
---
+++ Semana-2/ExemplosPraticos/Program.cs
@@ -0,0 +1,56 @@
+﻿// Escreva um programa em c# que imprime todos os números que são divisiveis por 3 ou 4 entre 
+// 0 e 30
+
+#region exemplo 1
+for (int i = 0; i <= 30; i++)
+{
+    if (i % 3 == 0 || i % 4 == 0)
+{
+    Console.WriteLine(i);
+}
+}
+#endregion
+
+// Faça um programa em c# que imprima os primeiros números da série de finonacci até passar 
+// de 100
+
+#region exemplo 2
+int a = 0, b = 1, c = 0;
+
+while (c <= 100)
+{
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+    c = a + b;
+}
+#endregion
+
+// Faça um programa que imprima a seguinte tabela até o nível 8
+
+#region exemplo 3 
+// int linhas = 4;
+
+// for (int i = 4; i >= linhas; i--)
+// {
+//     for (int j = 1; j >= 8; j++)
+//     {
+//         Console.WriteLine($"[i * j]\t");
+//     }
+//     Console.WriteLine();
+// }
+#endregion
+
+#region 
+Console.WriteLine("Informe uma string");
+string? str = Console.ReadLine();
+Console.WriteLine($"A string foi:  {str}");
+
+
+string [] strSplitted = str.Split(' ');
+
+foreach(string s in strSplitted)
+{
+    Console.WriteLine($"A string foi: {strSplitted}");
+}
+#endregion
